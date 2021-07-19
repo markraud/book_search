f8f49c35ddf1c9a77f71b4f3307c8c0150f725a6
Mark Raudenbush <markraud@hotmail.com>
2021-07-18 19:53:41 -0600

    cleaned up SingupForm.js and was able to get signup new user to work. Login with an existing user still doesn't work

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -40,21 +40,6 @@ const SignupForm = () => {
       console.error(err);
     }
 
-
-    //   const response = await createUser(userFormData);
-
-    //   if (!response.ok) {
-    //     throw new Error('something went wrong!');
-    //   }
-
-    //   const { token, user } = await response.json();
-    //   console.log(user);
-    //   Auth.login(token);
-    // } catch (err) {
-    //   console.error(err);
-    //   setShowAlert(true);
-    // }
-
     setUserFormData({
       username: '',
       email: '',
