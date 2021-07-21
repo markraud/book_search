c1bcb616b7528c4395dcc862f64f6e466fcf14a0
Mark Raudenbush <markraud@hotmail.com>
2021-07-20 22:01:06 -0600

    deploy

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -76,6 +76,7 @@ const SearchBooks = () => {
       const { data } = await saveBook({
         variables: { bookData: { ...bookToSave } }
       });
+      console.log(data);
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
